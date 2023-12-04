6bb9a403938f85a7bab4c4e7ee1e973493fd8815
LoKe112 <ivan1200100@mail.ru>
2023-12-04 17:26:19 +0400

    year and week cut fixes

--- year.py
+++ year.py
@@ -1,6 +1,8 @@
 import csv
 import time
 
+from datetime import *
+from typing import Iterable
 
 def N_cut_by_year(path: str) -> None:
     """Open .csv file from path and fill "2/" folder with n files : 1 file = 1 year
@@ -8,19 +10,21 @@ def N_cut_by_year(path: str) -> None:
     Args:
         path (str): path to file to cut
     """
-    for year in range(2008, 2024):
-        data = []
-        with open(path, "r", encoding="utf-8", newline="") as file:
-            reader = csv.reader(file)
-            for row in reader:
-                if str (year) in row[0]:
-                    data.append(row)
-        time.sleep(0.1)
-        with open(f"2\\{year}-{year+1}.csv", "w", encoding="utf-8", newline="") as file_N:
-            writer = csv.writer(file_N)
-            writer.writerows(data)
-        time.sleep(0.1)
-    print("end")
+    with open(path, "r", encoding="utf-8", newline="") as file:
+        reader = csv.reader(file)
+        data: dict[int,list[Iterable]]
+        data = {}
+        for row in reader:
+            d=datetime.strptime(row[0],"%Y-%m-%d")
+            if d.year not in data:
+                data[d.year]=[]
+            data[d.year].append(row)
+        for year in data.keys():
+            start_date=data[year][0][0].replace("-","")
+            end_date=data[year][-1][0].replace("-","")
+            with open(f"2\\{start_date}_{end_date}.csv" , "w", encoding="utf-8", newline="") as file_N:
+                writer = csv.writer(file_N)
+                writer.writerows(data[year])
 
 
 def main() -> None:
